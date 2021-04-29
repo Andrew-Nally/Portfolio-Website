219450b20f65f6f54a3df670bead754b01da0eb1
Andrew-Nally <Andrew_Nally@outlook.com>
2021-04-29 00:43:11 -0700

    April29th--updated

--- main.js
+++ main.js
@@ -1,27 +1,5 @@
-const bodyElement = document.querySelector('body');
-const menuToggle = document.querySelector(".hamburger-menu");
-const heroElement = document.querySelector(".hamburger-menu");
-const navList = document.querySelector('.nav-list');
-
-menuToggle.addEventListener('click', function (){
-    bodyElement.classList.toggle('is-open');
-    heroElement.classList.add('is-open');
-});
-
-navList.addEventListener('click', function(e){
-    let clickedElement = e.target;
-
-    if (clickedElement.classList[0] === "nav-link"){
-
-    }
-
-});
-
-window.addEventListener('click', function (e){
-    let clickedElement = e.target;
-
-      if (clickedElement.matches('.is-open')){
-          bodyElement.classList.remove('is-open');
-          heroElement.classList.remove('is-open');
-      }
+$(document).ready(function(){
+  $(".nav-item").click(function(){
+    $("#nameHeader").slideUp();
+  });
 });
